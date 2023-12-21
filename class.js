8da56ece552b789635bcbdd7ef3c9b85202fe6ce
Mrunali Kale <mrunalikale76@gmail.com>
2023-12-21 16:37:20 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,44 @@
+//ES6
+
+class User{
+    constructor(username, email,password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+
+}
+
+const chai = new User("chai", "chai@gamil.com", "123")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
+
+//behind the scene
+
+function User2(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
+
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+
+}
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
+
