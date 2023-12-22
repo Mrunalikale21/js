87bca862ce4003ec554e0e28072114eb616bc578
Mrunali Kale <mrunalikale76@gmail.com>
2023-12-23 04:02:48 +0530

    Add files via upload

---
+++ controlflow.js
@@ -0,0 +1,50 @@
+//if
+//scope of var is completely global
+const isUserloggedIn = true
+const temperature = 41
+
+if(temperature < 50){
+   console.log("less than 50");
+}else{
+console.log("temperature is greater than 50");
+}
+
+const score = 200
+
+if(score > 100){
+    var power = "fly"
+    console.log(`user power:${power}`);
+}
+
+console.log(`user power:${power}`); //it also excute because var has global scope
+
+const balance = 1000
+
+//if(balance>500) console.log("test"); //implicit scope
+
+if(balance < 500){
+    console.log("less than 500");
+
+} else if(balance < 750){
+    console.log("less than 750");
+
+}else if(balance < 900){
+    console.log("less than 900");
+
+}else{
+    console.log("less than 1200");
+}
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFRomEmail = true
+
+if(UserLoggedIn && debitCard){
+    console.log("allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromGoogle ){
+    console.log("user logged in");
+}
+
