62261255f4a87fd59e1d632c80de230d3d85c611
Mrunali Kale <mrunalikale76@gmail.com>
2023-12-24 13:06:16 +0530

    Add files via upload

---
+++ controlflow1.js
@@ -0,0 +1,37 @@
+//switch
+//switch(key){
+//    case value:
+//        break;
+
+//    default:
+//        break;
+//}
+
+const month = 3
+
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+       
+    case 2:
+        console.log("feb");
+        break;
+       
+    case 3:
+        console.log("march");
+        break;
+
+    case 4:
+        console.log("april");
+        break;
+
+        default:
+            console.log("defult case match");
+            break;
+       
+}
+
+//if we comment out break then where case matches code after it will run except default condition so it is important to write break condition
+//break will break control flow
+
