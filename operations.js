918d4ea2bfe01122da75e13390b7113f476d7855
Mrunali Kale <mrunalikale76@gmail.com>
2024-01-17 16:38:48 +0530

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,40 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "mrunali"
+let str2 = " kale"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 );
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+//comparision
+console.log("2" > 1); //true string convert into number
+
+console.log(null > 0);
+console.log( null == 0);
+console.log( null >= 0); //it is different bcoz comparison convert datatype of null into zero 
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+//strict check === it check datatype also
+console.log("2" === 2);
+
+
