02ab06a4721a3c69a1f35390206cb5d29c457f9b
Mrunali Kale <mrunalikale76@gmail.com>
2024-01-28 14:40:09 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "mrunalikale76@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+console.log(accountId);
+
+accountEmal = "mrunau@ali"
+accountPassword = "23448391"
+accountCity = "nagpur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
